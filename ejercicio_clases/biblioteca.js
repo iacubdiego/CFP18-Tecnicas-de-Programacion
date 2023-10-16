a3a4adedc3ba395116a5cb17137c8da26815a6bf
igna-zanella <igna.zanella@gmail.com>
2023-10-16 02:27:25 -0300

    se soluciona funcionamiento en el constructor

--- ejercicio_clases/biblioteca.js
+++ ejercicio_clases/biblioteca.js
@@ -2,18 +2,23 @@ class Biblioteca {
 
 }
 class Libro {
+   
+    //titulo = this.titulo;// "harry potter";
+    //autor = ""; // " J.K. Rowling";
+    //genero = ""; // "Fantasía";
     constructor(titulo,autor,genero){
-    titulo=this.titulo;// "jarri pote";
-    autor=this.autor; // " J.K Rowling";
-    genero=this.genero; // "Fantasía";
+    this.titulo = titulo;// "harry potter";
+    this.autor = autor; // " J.K. Rowling";
+    this.genero = genero; // "Fantasía";
 }
-    mostrarse(){
+    mostrarse(titulo){
         return console.log(`EL título de su libro es ${this.titulo}, autor ${this.autor}, y su género es ${this.genero}`)
     
     }
 }
 const libro1 = new Libro("Harry Potter", "J.K. Rowling", "Fantasía")
-const libro2 = new Libro("El viejo y el mar", "Ernst Heminghway", "Aventura")
+const libro2 = new Libro("El viejo y el mar", "Ernest Hemingway", "Aventura")
 //libro1.mostrarse(titulo,autor,genero);
+libro1.mostrarse();
 libro2.mostrarse();
 //console.log(libro1.mostrarse(titulo,autor,genero))
