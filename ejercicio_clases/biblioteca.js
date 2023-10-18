46bbde9a6f76e5e5d3517bf4b09260688b64f68b
igna-zanella <igna.zanella@gmail.com>
2023-10-17 21:39:29 -0300

    cambios en ejercicio biblioteca

--- ejercicio_clases/biblioteca.js
+++ ejercicio_clases/biblioteca.js
@@ -2,17 +2,18 @@ class Biblioteca {
 
 }
 class Libro {
-   
-    //titulo = this.titulo;// "harry potter";
-    //autor = ""; // " J.K. Rowling";
-    //genero = ""; // "Fantasía";
-    constructor(titulo,autor,genero){
-    this.titulo = titulo;// "harry potter";
-    this.autor = autor; // " J.K. Rowling";
-    this.genero = genero; // "Fantasía";
+   stock = "En stock"
+   //titulo = this.titulo;// "harry potter";
+   //autor = ""; // " J.K. Rowling";
+   //genero = ""; // "Fantasía";
+   constructor(titulo,autor,genero){
+       this.titulo = titulo;// "harry potter";
+       this.autor = autor; // " J.K. Rowling";
+       this.genero = genero; // "Fantasía";
+    //    this.stock = "En stock"
 }
     mostrarse(titulo){
-        return console.log(`EL título de su libro es ${this.titulo}, autor ${this.autor}, y su género es ${this.genero}`)
+        return console.log(`EL título de su libro es ${this.titulo}, autor ${this.autor}, y su género es ${this.genero}(${this.stock})`)
     
     }
 }
@@ -21,4 +22,5 @@ const libro2 = new Libro("El viejo y el mar", "Ernest Hemingway", "Aventura")
 //libro1.mostrarse(titulo,autor,genero);
 libro1.mostrarse();
 libro2.mostrarse();
+console.log(libro1)
 //console.log(libro1.mostrarse(titulo,autor,genero))
